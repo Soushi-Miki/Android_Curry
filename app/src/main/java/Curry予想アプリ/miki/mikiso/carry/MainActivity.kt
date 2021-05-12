676be4afb252817ef186bbd4be6ef283154a7cd4
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-05-12 22:56:42 +0900

    よそう?よそわない?アプリ_05 完成

--- app/src/main/java/Curry予想アプリ/miki/mikiso/carry/MainActivity.kt
+++ app/src/main/java/Curry予想アプリ/miki/mikiso/carry/MainActivity.kt
@@ -2,6 +2,7 @@ package Curry予想アプリ.miki.mikiso.carry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Size
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,11 +12,15 @@ class MainActivity : AppCompatActivity() {
 
     val people = arrayOf("大山","藤浪","梅野","津田")
     val food = arrayOf("かれー","みそしる","ちゃーはん","えびちり","からあげ","スープ")
+    val menu = arrayOf("よそえました^^","よそえませんでした")
+    val textsize = arrayOf(12F,30F)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        verbTextView.textSize = 18F
     }
+
     fun serveFood(view: View?){
 
         //Randomクラスの新しいインスタンスを初期化
@@ -25,10 +30,9 @@ class MainActivity : AppCompatActivity() {
         nameTextView.text = name + "に"
         foodTextView.text = food[Random.nextInt(6)] + "を"
 
-        if(Random.nextInt(100) < 50){
-            verbTextView.text = "よそえました^^"
-        }else{
-            verbTextView.text = "よそえませんでした"
-        }
+        val index1 = randomName.nextInt(2)
+        verbTextView.text = menu[index1]
+        verbTextView.textSize = textsize[index1]
+
     }
 }
